85a1b4714afe2fb9fb342672b845f10f29e08ad6
unknown <dcavadia@shokworks.io>
2020-11-06 00:06:20 -0400

    init

---
+++ Dubai 3D Weather/Assets/Scripts/Weather.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour
+{
+    // Private Singleton-style instance. Accessed by static property S later in script
+    static private Weather _S;
+    static private eWeatherState WEATHER_STATE = eWeatherState.none;
+
+    [System.Flags]
+    public enum eWeatherState
+    {
+        // Decimal      // Binary
+        none = 0,       // 00000000
+        sunny = 1,      // 00000001
+        cloudy = 2,     // 00000010
+      //rainy = 4,         00000100
+    }
+
+    [Header("Set in Inspector")]
+    [Tooltip("This sets the WeatherScriptableObject to be used throughout the app.")]
+    public WeatherScriptableObject weatherSO;
+
+    [SerializeField]
+    [Tooltip("This private field shows the weather state in the Inspector")]
+    protected eWeatherState weatherState;
+
+    private void Awake()
+    {
+        S = this;
+        weatherState = eWeatherState.none;
+        WEATHER_STATE = weatherState;
+    }
+
+    void WeatherStateChanged()
+    {
+        this.weatherState = Weather.WEATHER_STATE;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // ---------------- Static Section ---------------- //
+
+    //This static private property provides some protection for the Singleton _S
+    static private Weather S
+    {
+        get
+        {
+            if (_S == null)
+            {
+                Debug.LogError("Weather:S getter - Attempt to get value of S before it has been set.");
+                return null;
+            }
+            return _S;
+        }
+        set
+        {
+            if (_S != null)
+            {
+                Debug.LogError("Weather:S setter - Attempt to set S when it has already been set.");
+            }
+            _S = value;
+        }
+    }
+
+    static public WeatherScriptableObject WeatherSO
+    {
+        get
+        {
+            if (S != null)
+            {
+                return S.weatherSO;
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ Dubai 3D Weather/Assets/Scripts/WeatherScriptableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/WeatherSO", fileName = "WeatherSO.asset")]
+[System.Serializable]
+public class WeatherScriptableObject : ScriptableObject
+{
+    static public WeatherScriptableObject S; // This Scriptable Object is an unprotected Singleton
+    public WeatherScriptableObject()
+    {
+        S = this; // Assign the Singleton as part of the constructor.
+    }
+
+    public float weatherScale = 50f;
+
+    public GameObject[] weatherPrefabs;
+
+    public GameObject GetWeatherPrefab()
+    {
+        int ndx = 0;
+        return weatherPrefabs[ndx];
+    }
+
+
+}
